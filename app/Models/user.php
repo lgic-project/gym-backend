1a5aae5049c2696a0dec5dafffada7f0c0cd1602
anish tiwari <ani.tiw.45@gmail.com>
2024-06-27 10:13:53 +0545

    Refactor User model: roles as constants, fix subscription details attribute, add comments.

--- app/Models/user.php
+++ app/Models/user.php
@@ -2,8 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -16,7 +14,8 @@ class User extends Authenticatable implements HasMedia
 {
     use HasApiTokens, HasFactory, Notifiable, InteractsWithMedia;
 
-    public static $roles = [
+    // Define roles as a constant array
+    public const ROLES = [
         1 => 'Admin',
         2 => 'Trainer',
         3 => 'Customer',
@@ -25,7 +24,7 @@ class User extends Authenticatable implements HasMedia
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $fillable = [
         'name',
@@ -36,13 +35,13 @@ class User extends Authenticatable implements HasMedia
         'user_role',
         'description',
         'cost_per_month',
-        'experience'
+        'experience',
     ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $hidden = [
         'password',
@@ -52,50 +51,71 @@ class User extends Authenticatable implements HasMedia
     /**
      * The attributes that should be cast.
      *
-     * @var array<string, string>
+     * @var array
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
 
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
     protected $appends = [
-        'suscription_details',
+        'subscription_details', // corrected typo in the attribute name
         'bookings',
-        'photo'
+        'photo',
     ];
 
-
-    public function getSuscriptionDetailsAttribute()
+    /**
+     * Get the user's subscription details attribute.
+     *
+     * @return array
+     */
+    public function getSubscriptionDetailsAttribute()
     {
         $data = [];
-        $suscription = $this->suscriptions()->latest()->first();
-        if ($suscription) {
-            $data['total_days'] = Carbon::parse($suscription->valid_from)->diffInDays(Carbon::parse($suscription->valid_till));
-            $data['total_remaining_days'] = Carbon::parse($suscription->valid_till)->diffInDays(Carbon::now());
-            $data['start_date'] = Carbon::parse($suscription->valid_from)->format('d M Y');
-            $data['end_date'] = Carbon::parse($suscription->valid_till)->format('d M Y');
+        $subscription = $this->subscriptions()->latest()->first();
+        if ($subscription) {
+            $data['total_days'] = Carbon::parse($subscription->valid_from)->diffInDays(Carbon::parse($subscription->valid_till));
+            $data['total_remaining_days'] = Carbon::parse($subscription->valid_till)->diffInDays(Carbon::now());
+            $data['start_date'] = Carbon::parse($subscription->valid_from)->format('d M Y');
+            $data['end_date'] = Carbon::parse($subscription->valid_till)->format('d M Y');
         }
         return $data;
     }
 
-
-    public function suscriptions()
+    /**
+     * Get the user's subscriptions.
+     */
+    public function subscriptions()
     {
-        return $this->hasMany(Suscription::class);
+        return $this->hasMany(Subscription::class);
     }
 
+    /**
+     * Get the user's bookings attribute.
+     *
+     * @return mixed
+     */
     public function getBookingsAttribute()
     {
         if ($this->user_role == 2) {
             return Booking::where('trainer_id', $this->getKey())->paginate(20);
-        } else if ($this->user_role == 3) {
+        } elseif ($this->user_role == 3) {
             return Booking::where('client_id', $this->getKey())->paginate(20);
         } else {
             return Booking::paginate(20);
         }
     }
 
+    /**
+     * Get the user's photo attribute.
+     *
+     * @return string
+     */
     public function getPhotoAttribute()
     {
         if ($this->hasMedia()) {
